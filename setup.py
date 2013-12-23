d12e41e2e88249e2fd9de72be35f944b622f45a4
Maxim Stolyarchuk <maxim.stolyarchuk@gmail.com>
2013-12-24 03:02:24 +0400

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Staticify',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/mstolyarchuk/flask-staticify',
     license='BSD',
     author='Maxim Stolyarchuk',
